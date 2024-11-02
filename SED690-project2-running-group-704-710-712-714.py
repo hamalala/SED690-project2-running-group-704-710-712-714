4c3dfb9779944f559f01648b0433b49fac48a65e
hamalala <46687725+hamalala@users.noreply.github.com>
2024-11-03 00:42:21 +0700

    Update SED690-project2-running-group-704-710-712-714.py

--- SED690-project2-running-group-704-710-712-714.py
+++ SED690-project2-running-group-704-710-712-714.py
@@ -13,7 +13,7 @@ st.write("\n\n")
 # Check if the model is already in session state
 if 'model' not in st.session_state:
     # Load default model from a pickle file
-    default_model_path = '/mount/src/sed690-project2-running-group-704-710-712-714/RandomForest with out imbalance.pkl'  # Replace with your model file path
+    default_model_path = '/mount/src/sed690-project2-running-group-704-710-712-714/default.pkl'  # Replace with your model file path
     st.session_state.model = load_model(default_model_path)
     st.session_state.model_loaded = True
     st.success("Loaded default model.")
@@ -41,12 +41,25 @@ if st.session_state.get('model_loaded', False):
     
     st.write("**Features**")
     label_encoders = st.session_state.model['label_encoders']
+    data_types = st.session_state.model['data_types']
 
-    for feature in st.session_state.model['features'] :    
+    for feature in st.session_state.model['features'] :
+        data_type = data_types[feature] 
         if feature in label_encoders :
             original_labels = label_encoders[feature].classes_
             selected_label = st.selectbox("Choose value for {feature}:", original_labels, key=feature)
             input_values[feature] = selected_label  # Store the input value in a dictionary
+        elif len(data_type['Unique Values']) > 0 :
+            selected_label = st.selectbox("Choose value for {feature}:", data_type['Unique Values'], key=feature)
+            input_values[feature] = selected_label  # Store the input value in a dictionary
+        elif data_type['Data Type'] in ['int64', 'float64']:
+            # Create a number input for this column
+            input_value = st.number_input(
+                label=f"Enter value for {feature}:",
+                format="%f" if data_type['Data Type'] == 'float64' else "%d", 
+                key=feature
+            )
+            input_values[feature] = input_value  # Store the input value in a dictionary
         else:
             input_value = st.text_input(f"Enter value for {feature}:", key=feature)
             input_values[feature] = input_value  # Store the input value in a dictionary
